3f73ce94b8582f1909c1eae8b93014d121e9b250
Rezzadty <rezaadityaprabowo474@gmail.com>
2026-01-15 04:44:18 +0700

    Adding Event Controller, Detail Event Page and editing Homepage and routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,17 @@
 use App\Http\Controllers\User\HomeController;
 use App\Http\Controllers\Admin\HistoriesController;
 use App\Http\Controllers\Admin\TiketController;
-use App\Http\Controllers\Admin\EventController;
+use App\Http\Controllers\Admin\EventController as AdminEventController;
+use App\Http\Controllers\User\EventController as UserEventController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+// Events
+Route::get('/events/{event}', [UserEventController::class, 'show'])->name('events.show');
 
 
 Route::middleware('auth')->group(function () {
@@ -24,7 +28,7 @@ Route::middleware('auth')->group(function () {
         Route::resource('categories', CategoryController::class);
 
         // Event Management
-        Route::resource('events', EventController::class);
+        Route::resource('events', AdminEventController::class);
 
         // Tiket Management 
         Route::resource('tickets', TiketController::class);
